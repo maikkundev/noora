0de2901085a9a3b2bffdbd20b7a4cf215c54354e
Michael Avagianos <hello@maikkun.dev>
2024-10-23 17:21:31 +0300

    feat: add base speed

--- entities/entity.gd
+++ entities/entity.gd
@@ -2,12 +2,14 @@
 class_name Entity
 extends CharacterBody2D
 
+@export var base_speed: float
+
 ## The type of entity this is.
 @export var entity_type: EntityType
 
 ## Returns the type of the entity.
 func get_entity_type() -> EntityType:
-    return entity_type
+	return entity_type
 
 func _ready():
-    pass
+	base_speed = entity_type.base_speed
