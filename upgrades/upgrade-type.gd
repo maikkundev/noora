80cc18e25324dbfe0bed2d6cd430a9c5521458c0
Michael Avagianos <hello@maikkun.dev>
2024-12-13 21:24:16 +0200

    feat:add upgrade type

---
+++ upgrades/upgrade-type.gd
@@ -0,0 +1,7 @@
+class_name UpgradeType
+extends Resource
+
+@export_group("General")
+
+# Name of the upgrade
+@export var name: String
---
+++ upgrades/upgrade.gd
@@ -0,0 +1,5 @@
+class_name Upgrade
+extends UpgradeType
+
+func _process(_delta: float) -> void:
+	pass
